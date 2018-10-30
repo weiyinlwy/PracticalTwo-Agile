5316e4e72909ce80b454371476e26e3162d77751
Tai Jia Hao <44257740+Dangko2218@users.noreply.github.com>
2018-10-30 14:53:05 +0800

    Update Practical2Agile.java

--- src/practical2/agile/Practical2Agile.java
+++ src/practical2/agile/Practical2Agile.java
@@ -17,6 +17,7 @@ public class Practical2Agile {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!!!!");
+        System.out.println("I Love U");
     }
     
 }
